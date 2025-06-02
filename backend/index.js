b6533b158b8108304565dde3a5ad80c3b143f446
Jeffery Wu <jefferyl.wu@gmail.com>
2025-06-02 09:16:06 -0400

    Merged frontend with main

--- backend/index.js
+++ backend/index.js
@@ -2,19 +2,27 @@ import express from "express";
 import cors from "cors" //to enable CORS for the backend server
 import connectDB from './configs/db.js';
 import 'dotenv/config'; //to load environment variables from .env file
+import authRoutes from './routes/authRoutes.js'; //importing auth routes
 
 
-dotenv.config(); //load environment variables from .env file
-const app = express();
 
 connectDB();
+//load environment variables from .env file
+const app = express();
+app.use(cors())
+
+app.use(express.json()) //allows us to parse incoming requests: req.body
+
 
 const PORT = process.env.PORT || 3000;
 
-app.use(cors()); //enable communication between frontend and backend
+//API endpoints for testing
 app.get('/', (req, res) => {
     res.send('Hello, World');
 })
+app.use('/api/auth', authRoutes)
 app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}`);
+    console.log()
+
 })      
---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,20 @@
+import JWT from 'jsonwebtoken';
+
+module.exports =async (req, res, next) => {
+    try{
+        const token = req.headers["authorization"].split(" ")[1];
+        JWT.verify(token, process.env.JWT_SECRET, (err, decode) => {
+            if(err){
+                return res.status(200).send({
+                    success: false,
+                    message: "Invalid token",
+                })
+            }
+        })
+    }catch(error){
+        return res.status(500).send({
+            success: false,
+            message: "Server error",
+        })
+    }
+}
